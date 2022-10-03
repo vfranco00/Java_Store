4d9c8e6aa36f2aeee08d633c923a95e397675341
vfranco00 <victorfranco02@outlook.com>
2022-10-02 23:09:39 -0300

    [Feat] - Starting Project

---
+++ src/main/java/com/mycompany/java_store/Inventory.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.java_store;
+
+/**
+ *
+ * @author victo
+ */
+public class Inventory {
+    private int id;
+    private String productName;
+    private int quantity;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
