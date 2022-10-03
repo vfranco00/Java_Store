9258492a8d842276d6c3fd3801d3a5255ed52ef4
vfranco00 <victorfranco02@outlook.com>
2022-10-02 23:15:49 -0300

    [Fix] Trying to reconnect

---
+++ src/main/java/com/mycompany/java_store/Customers.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.java_store;
+
+/**
+ *
+ * @author victo
+ */
+public class Customers {
+    private int id;
+    private String customerName;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+    
+    
+}
---
+++ src/main/java/com/mycompany/java_store/Shipping_Company.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.java_store;
+
+/**
+ *
+ * @author victo
+ */
+public class Shipping_Company {
+    private int id;
+    private String companyName;
+    private double shippingValue;
+    private int quantity;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public double getShippingValue() {
+        return shippingValue;
+    }
+    public void setShippingValue(double shippingValue) {
+        this.shippingValue = shippingValue;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    
+    
+}
