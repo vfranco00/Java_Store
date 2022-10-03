81fdadfbd04811a9e72550a8acf5e43b17d89195
vfranco00 <victorfranco02@outlook.com>
2022-10-02 23:12:17 -0300

    [Feat] Provider Creation

---
+++ src/main/java/com/mycompany/java_store/Provider.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.java_store;
+
+/**
+ *
+ * @author victo
+ */
+public class Provider {
+    private int id;
+    private String productName;
+    private String provider;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return productName;
+    }
+
+    public void setName(String newProductName) {
+        this.productName = newProductName;
+    }
+
+    public String getProvider() {
+        return provider;
+    }
+
+    public void setProvider(String provider) {
+        this.provider = provider;
+    }
+}
